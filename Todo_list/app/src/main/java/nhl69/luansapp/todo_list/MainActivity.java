36cf392c86fa53e4b1be1b52e7bbadf6d80f41d7
hailuan609 <nguyenhailuan609@gmail.com>
2023-01-13 10:47:19 +0700

    Lay 1

---
+++ Todo_list/app/src/main/java/nhl69/luansapp/todo_list/MainActivity.java
@@ -0,0 +1,45 @@
+package nhl69.luansapp.todo_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation top_animation, bottom_animation;
+    ImageView img;
+    TextView textView;
+    private static final int SPLASH = 2200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.image_view);
+        textView = findViewById(R.id.title);
+
+        top_animation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottom_animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        img.setAnimation(top_animation);
+        textView.setAnimation(bottom_animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH);
+    }
+}
